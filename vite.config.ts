b80c70141884030db266afd73497dddc18ba44e7
Elmo <219951881+mlbb-toolkit@users.noreply.github.com>
2025-07-09 19:36:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/mltoolkit/',
+  base: '/mlbb-toolkit.github.io/',
   plugins: [react()],
 });
