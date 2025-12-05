ce5e53de85e1827f59262ab56dcf961e7a9c8fed
elfilibusterismo <113604229+elfilibusterismo@users.noreply.github.com>
2025-12-05 12:39:42 +0000

    Deploying to gh-pages from @ mlbb-toolkit/mlbb-toolkit.github.io@948f980b2954411ac1494ed11291718d1e198797 🚀

--- main.dart.js
+++ main.dart.js
@@ -21962,7 +21962,7 @@ $iDa:1}
 A.aaz.prototype={
 $1(a){var s=A.cf().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/13e658725ddaa270601426d1485636157e38c34c/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/a5cb96369ef86c7e85abf5d662a1ca5d89775053/":s)+a},
 $S:157}
 A.Dq.prototype={
 gt(a){var s=this.a
