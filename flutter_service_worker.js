b2179aa09333c95de240fb85af81d9bb110ee719
elfilibusterismo <113604229+elfilibusterismo@users.noreply.github.com>
2025-12-12 01:23:49 +0000

    Deploying to gh-pages from @ mlbb-toolkit/mlbb-toolkit.github.io@f021bcabc83126c321e770fab4147d697c9cd35e 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -45,7 +45,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.ico": "391f76debe9008edcb49204d353b5fb4",
 "404.html": "1ac36acce302f8d45ef9565f74593bcf",
-"flutter_bootstrap.js": "3e8f5e73e74db4d854db003687687d29",
+"flutter_bootstrap.js": "c11faf236cca201df6e81df79cc68017",
 "version.json": "0ceec88624f0ef21e91edafad840cad7",
 "main.dart.js": "b6f7dcc7bef0cc6885d29568f548c19a"};
 // The application shell files that are downloaded before a service worker can
