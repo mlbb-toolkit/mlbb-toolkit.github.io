48c826b8a20c8605cabc5e64ba830a235ffc3219
mlbb-toolkit <devcloper.url@gmail.com>
2026-02-03 08:51:32 +0000

    Deploying to gh-pages from @ mlbb-toolkit/mlbb-toolkit.github.io@3a8276e163d09dd9082b6aff65c9d4a246540087 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2540526823"
+    serviceWorkerVersion: "1949740463"
   }
 });
